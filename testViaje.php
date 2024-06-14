4f664cf9cef7861c2265abc8bfac6732c5594225
JoaquinIVL95 <joaquinIVl95@gmail.com>
2024-06-14 19:49:35 -0300

    Agregue archivo para los test, testViaje, segun los enunciados y modifique el archivo Responsable con sus datos y metodos de acceso

---
+++ testViaje.php
@@ -0,0 +1,14 @@
+<?php
+
+
+include 'BaseDatos.php';
+include 'Empresa.php';
+include 'Responsable.php';
+include 'Viaje.php';
+include 'Pasajero.php';
+
+
+$persona = new Pasajero();
+$persona->Buscar('12345678');
+echo $persona;
+
